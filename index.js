b570c368e0069da5f3762b57c371d2650e84a6cf
Thiago Dorneles <thiagodorneles@Thiagos-MacBook-Pro.local>
2016-05-16 18:00:31 -0300

    new resource

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
+app.get('/test', function(req, res) {
+    res.send('Funcionando CI')
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
